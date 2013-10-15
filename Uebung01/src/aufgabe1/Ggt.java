8844329968f6e94d4cdad4c57f8438e5061db5c6
celentanos <barlachstrasse@mail.ru>
2013-10-15 23:55:12 +0200

    last

--- Uebung01/src/aufgabe1/Ggt.java
+++ Uebung01/src/aufgabe1/Ggt.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class Ggt {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-Scanner input = new Scanner(System.in);		
-		
-int n , m , r;
-System.out.print("x = ");
-int x = input.nextInt();
-System.out.print("y = ");
-int y = input.nextInt();
-n = x; m = y;
-r = n % m;
-while (r!=0){
-	n = m;
-	m = r;
-	r = n % m;
-}
-System.out.println( "Der ggT von "+ x +" und "+ y +" ist "+ m);
+
+		Scanner input = new Scanner(System.in);
+
+		int n, m, r;
+		System.out.print("x = ");
+		int x = input.nextInt();
+		System.out.print("y = ");
+		int y = input.nextInt();
+		n = x;
+		m = y;
+		r = n % m;
+		while (r != 0) {
+			n = m;
+			m = r;
+			r = n % m;
+		}
+		System.out.println("Der ggT von " + x + " und " + y + " ist " + m);
 	}
 }
